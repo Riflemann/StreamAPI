7e65ce2e659c4b695b06f78025761acc3a0d3f5a
Konstantin <kdanilov1911@gmail.com>
2023-03-09 23:30:35 +0300

    HW is done

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ import java.util.stream.Stream;
 public class Main {
     public static List<Integer> integers
             = new ArrayList<>(List.of(
-            1,2,3,4,5,6,7,8,9,10,
-            11,12,13,14,15,16,17,18,19,20));
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
 
-    public static Long getCountForEvenInteger(List<Integer> integers){
-        return integers.stream().filter(integer->integer % 2 == 0).count();
+    public static Long getCountForEvenInteger(List<Integer> integers) {
+        return integers.stream().filter(integer -> integer % 2 == 0).count();
     }
 
     public static <A> void findMinMax(
@@ -32,11 +32,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("\n Quantity for even integers in list: " + getCountForEvenInteger(integers) +"\n");
+        System.out.println("\n Quantity for even integers in list: " + getCountForEvenInteger(integers) + "\n");
 
         Stream<Integer> stream = integers.stream();
 
         findMinMax(stream, Integer::compareTo, (x, y) -> System.out.printf("min: %s, max: %s%n", x, y));
 
         stream.close();
+    }
 }
